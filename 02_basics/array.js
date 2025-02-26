b295368e15d0d06a59a2917952b8c96f818262ac
uday pandit <144710930+udayaryan0001@users.noreply.github.com>
2025-02-26 05:01:56 +0000

    a code repo for js

---
+++ 02_basics/array.js
@@ -0,0 +1,20 @@
+const myArr= [0,1, 2, 3, 4, 5];
+console.log(myArr[0]);
+  //output is 1
+const myheors =["shaktiman" ,"naagraj"];
+const myarr2= new Array(1,2,3)
+  // output [ 1, 2, 3 ]
+console.log(myarr2)
+ //.length tells the length of array
+
+///Array methods
+myArr.push(6);
+console.log(myArr);
+
+myArr.pop();
+  //removes the last value directly
+
+myArr.unshift(9);
+ //It insert 9 at the begining
+
+ myArr.shift();
--- basic/09_dates.js
+++ basic/09_dates.js
@@ -1,13 +1,28 @@
-//dates
+ //dates
 
-let myDate=new Date();
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);  
+// let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);  
 //date is a object in js
 
-let mycreatedDate=new Date(2023,0,23,5,3);
+let mycreatedDate=new Date("01-14-2023")
 //console.log(mycreatedDate.toDateString())
-console.log(mycreatedDate.tolocalString())
+// console.log(mycreatedDate.tolocalString())
 
+let mytimestamp =Date.now()/1000;  //convert mille second to second 
+console.log(mytimestamp)    // 1/jan/1970  //ye ab tak ke mille second dega yeh
+//toh essa yeh hame date nukalne pade ge 
+
+console.log(mycreatedDate.getTime());
+
+let myDate=new Date();
+console.log(newDate)
+console.log(newDate.getMonth());
+console.log(newDate.getMonth()); 
+
+newDate.toLocalString('default',{
+    weekday: "long"
+    
+})
