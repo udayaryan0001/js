d91f99dc3b02554892d46e7e25f0744a4e00c64e
uday pandit <144710930+udayaryan0001@users.noreply.github.com>
2025-03-08 07:08:57 +0000

    a code repo for js

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -18,3 +18,27 @@ myArr.unshift(9);
  //It insert 9 at the begining
 
  myArr.shift();
+ // console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+//slice
+// like 1,3. => 3 is not include
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+//splice 
+//it almost same as the place but esma exting arrar ko he modified kar dega or line 1,3 kare ga toh 3 include rahe ga 
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
